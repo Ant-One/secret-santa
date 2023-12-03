36b80686c316e01d6e8aab5b1469ef4ea82c9113
Antoine Moix <antoine.moix@gmail.com>
2023-12-03 23:12:15 +0100

    feat(models): added models and todo

--- santa/app/models.py
+++ santa/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Draw(models.Model):
+    name = models.TextField(blank=False)
+
+class Pairing(models.Model):
+    participant_1 = models.ForeignKey("Participant", null=True)
+    participant_2 = models.ForeignKey("Participant", null=True)
+    draw = models.ForeignKey("Draw", null=False)
+
+class Participant(models.Model):
+    in_draw = models.ForeignKey("Draw", on_delete=models.SET_NULL, null=True)
+    name = models.TextField(blank=False)
+    pairing = models.ForeignKey("Pairing", null=True)
---
+++ todo.md
@@ -0,0 +1,7 @@
+# UI
+- Draw name?
+
+# Models
+- Participant
+- Draw
+- Pairing
